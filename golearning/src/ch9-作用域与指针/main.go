ca9aa7d17bb0692b36869656e4d97c59e3fc0e6b
demon.wang <nanwanwang@hotmail.com>
2020-06-09 23:23:19 +0800

    demon#20200609_pogo

--- golearning/src/ch9-作用域与指针/main.go
+++ golearning/src/ch9-作用域与指针/main.go
@@ -1,23 +1,28 @@
 
 package main
 
-
 import "fmt"
 
 func main(){
 
-   var a,b,c int
-   a=10
-   b=20
-   c=a+b
-   fmt.Println(a,b,c)
+   //var a,b,c int
+   //a=10
+   //b=20
+   //c=a+b
+   //fmt.Println(a,b,c)
+   //
+   //var p=20
+   //var ip *int
+   //ip=&p
+   //fmt.Println(ip)
+   //fmt.Println(*ip)
+   //
+   //*ip=30
+   //fmt.Println(p)
 
-   var p=20
-   var ip *int
-   ip=&p
-   fmt.Println(ip)
-   fmt.Println(*ip)
-
-   *ip=30
-   fmt.Println(p)
+   x:=10
+   var p*int =&x
+   fmt.Println(x,p,*p)
+   *p+=30
+   fmt.Println(*p)
 }
---
+++ golearning/src/pogo/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"flag"
+	"pogo/common/logs"
+)
+var log=logs.Log
+func init()  {
+	flag.Parse()
+}
+func main(){
+  if flag.NArg()==0{
+  	log.Error("no video url parameter!")
+	  return
+  }
+	url:=flag.Arg(0)
+	log.Debug(url)
+}
